2c8720a1b181f21e037aaf89ae2384a69535f7ba
Daniela Lopez <230415@utxicotepec.edu.mx>
2024-11-05 14:14:59 -0600

    Practica 11
    Se añadio nueva informacion

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,21 @@
-const formularioLogin=(req,res)=>{
+const formularioLogin=(request,response)=>{
     res.render('auth/login',{
+        autenticado: false,
         //con la coma decimos que hay un segundo parámetro
-        page:"Ingresa tu "
+        page:"Ingresa a la plataforma "
     })
 };
 
 const formularioRegister=(request,response)=>{
     response.render('auth/register',{
+        page:"Crea una cuenta ..."
 
     })
 };
 
 const formularioPasswordRecovery = (request,response)=>{
     response.render('auth/passwordRecovery',{
+        page:"Recupera tu contraseña"
 
     })
 }
